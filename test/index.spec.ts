f714a6f7b7969461e81ce1b14307bf44470f25fb
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-08 03:17:41 +0200

    add tests --cov-100% 🧪 ..

---
+++ test/index.spec.ts
@@ -0,0 +1,47 @@
+import { assert, expect } from 'chai'
+import mongoose from 'mongoose'
+import { Db, MongoClient } from 'mongodb'
+import { connect } from '@mongodb-helpers/connect'
+
+import { healthCheck } from '../src'
+
+const MONGODB_URL = 'mongodb+srv://admin:admin@cluster0.ndtam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+describe('health-check module', () => {
+  it('should export health-check as function with 1 param', () => {
+    assert.strictEqual(typeof healthCheck, 'function')
+    assert.strictEqual(healthCheck.length, 1)
+  })
+
+  describe ('with mongodb driver', () => {
+    it('should health-check return `Connected` when connect correctly', async () => {
+      const client = await MongoClient.connect(MONGODB_URL)
+      const status = await healthCheck(client)
+      assert(status === 'Connected')
+      client.close()
+    })
+  
+    it('should health-check return `Disconnected` when don\'t connect correctly', async () => {
+      const client = await MongoClient.connect(MONGODB_URL)
+      client.close()
+      const status = await healthCheck(client)
+      assert(status === 'Disconnected')
+    })
+  })
+
+  describe ('with mongoose', () => {
+    it('should health-check return `Connected` when connect correctly', async () => {
+      const client = await mongoose.connect(MONGODB_URL)
+      const status = await healthCheck(client)
+      assert(status === 'Connected')
+      client.connection.close()
+    })
+  
+    it('should health-check return `Disconnected` when don\'t connect correctly', async () => {
+      const client = await mongoose.connect(MONGODB_URL)
+      client.connection.close()
+      const status = await healthCheck(client)
+      assert(status === 'Disconnected')
+    })
+  })
+})
